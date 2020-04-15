00d4559387a5c890b4f195561cb43c7a4e713f0a
HaveaKillerDay <petho.adam911@gmail.com>
2020-04-15 13:50:00 +0200

    map bugfix

--- src/com/codecool/termlib/Main.java
+++ src/com/codecool/termlib/Main.java
@@ -8,48 +8,12 @@ public class Main {
         Terminal terminal = new Terminal();
         boolean isGamePlaying = true;
         Player firstPlayer = new Player();
-        /* Map level1 = new Map(); */
-       /*  for (int col = 0; col < mapMultiDim[0].length ; col++) {
-            System.out.println();
-            for (int row = 0; row < mapMultiDim[col].length  ; row ++) {
-                partOfMap = Character.toString(map.charAt(index));
-                
-                if (index == keyOfMap && firstPlayer.x == col && firstPlayer.y == row) {                             
-                    mapMultiDim[col][row] = level1.dot;
-                    firstPlayer.setKey(true);
-                    index ++;
-                }
-                else if (index == loot && firstPlayer.x == col && firstPlayer.y == row) {                             
-                    mapMultiDim[col][row] = dot;
-                    firstPlayer.setSword(item);
-                    index ++;
-                }
-                else if (firstPlayer.x == col && firstPlayer.y == row)
-                {
-                mapMultiDim[col][row] = firstPlayer.body;
-                System.out.print(mapMultiDim[col][row]);
-                index++;
-                }
-                
-                else {
-                    if (partOfMap.equals(pipeOfMap)) {
-                        mapMultiDim[col][row] = "\u001B[35m" + partOfMap + "\u001B[0m";
-                        System.out.print(mapMultiDim[col][row]);
-                        index ++;
-                    } else {
-                        mapMultiDim[col][row] = partOfMap;
-                        System.out.print(mapMultiDim[col][row]);
-                        index ++;
-                    }
-                    }
-                }
-            } */
-        
+        Map level1 = new Map();
+        level1.main(firstPlayer);
         while (isGamePlaying) {
             if (firstPlayer.key) {
                 System.out.println(firstPlayer.VICTORY);
             }
-            /* System.out.println(firstPlayer.sword); */
             System.out.println("Which way u want to move?");
             Scanner sc = new Scanner(System.in);
             String move = sc.next();
@@ -82,7 +46,6 @@ public class Main {
             else if (firstPlayer.y > 56) {
                 firstPlayer.y = 56;
             }
-            Map level1 = new Map();
             level1.main(firstPlayer);
         }
     }
